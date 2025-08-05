be658ba5176c03d086423eba24a478b9f45569bc
Dhairya Soni <drax6055@gmail.com>
2025-08-05 17:58:20 +0530

    working on staff screen

--- Desktop/admin_mobile_app_ifloriana_22_07_2025/flutter_template_home/flutter_template/lib/ui/drawer/staff/staffDetailsScreen.dart
+++ Desktop/admin_mobile_app_ifloriana_22_07_2025/flutter_template_home/flutter_template/lib/ui/drawer/staff/staffDetailsScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_template/route/app_route.dart';
 import 'package:flutter_template/ui/drawer/staff/staffDetailsController.dart';
 import 'package:flutter_template/utils/app_images.dart';
+import 'package:flutter_template/utils/colors.dart';
 import 'package:get/get.dart';
 
 import '../../../wiget/loading.dart';
@@ -15,80 +16,85 @@ class Staffdetailsscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() {
-        if (controller.isLoading.value) {
-          return const Center(child: CustomLoadingAvatar());
-        }
+      body: RefreshIndicator(
+        color: primaryColor,
+        onRefresh: () async {
+          controller.getCustomerDetails();
+        },
+        child: Obx(() {
+          if (controller.isLoading.value) {
+            return const Center(child: CustomLoadingAvatar());
+          }
 
-        if (controller.staffList.isEmpty) {
-          return const Center(child: Text("No staff found"));
-        }
+          if (controller.staffList.isEmpty) {
+            return const Center(child: Text("No staff found"));
+          }
 
-        return ListView.builder(
-          itemCount: controller.staffList.length,
-          itemBuilder: (context, index) {
-            final staff = controller.staffList[index];
+          return ListView.builder(
+            itemCount: controller.staffList.length,
+            itemBuilder: (context, index) {
+              final staff = controller.staffList[index];
 
-            return Card(
-              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-              elevation: 3,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: ListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                leading: CircleAvatar(
-                    radius: 30, backgroundImage: AssetImage(AppImages.applogo)
-                    // backgroundImage: staff.image != null &&
-                    //         staff.image!.isNotEmpty
-                    //     ? NetworkImage(staff.image!)
-                    //     : const AssetImage("assets/default.png") as ImageProvider,
-                    ),
-                title: Text(
-                  staff.fullName ?? "Unknown",
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (staff.email != null) Text("Email: ${staff.email}"),
-                    if (staff.branchId?.name != null)
-                      Text("Branch: ${staff.branchId!.name}"),
-                    if (staff.serviceId != null && staff.serviceId!.isNotEmpty)
-                      Text(
-                        "Services: ${staff.serviceId!.map((s) => s.name ?? '').where((name) => name.isNotEmpty).join(', ')}",
+                child: ListTile(
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  leading: CircleAvatar(
+                      radius: 30, backgroundImage: AssetImage(AppImages.applogo)
+                      // backgroundImage: staff.image != null &&
+                      //         staff.image!.isNotEmpty
+                      //     ? NetworkImage(staff.image!)
+                      //     : const AssetImage("assets/default.png") as ImageProvider,
                       ),
-                  ],
+                  title: Text(
+                    staff.fullName ?? "Unknown",
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (staff.email != null) Text("Email: ${staff.email}"),
+                      if (staff.branchId?.name != null)
+                        Text("Branch: ${staff.branchId!.name}"),
+                    ],
+                  ),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit_outlined,
+                            color: primaryColor),
+                        onPressed: () {
+                          Get.to(() => Addnewstaffscreen(staff: staff));
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete_outline,
+                            color: primaryColor),
+                        onPressed: () {
+                          _confirmDelete(context, staff.sId ?? '', controller);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.edit, color: Colors.blue),
-                      onPressed: () {
-                        Get.to(() => Addnewstaffscreen(staff: staff));
-                      },
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.delete, color: Colors.red),
-                      onPressed: () {
-                        _confirmDelete(context, staff.sId ?? '', controller);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-      }),
+              );
+            },
+          );
+        }),
+      ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: primaryColor,
         onPressed: () {
           Get.toNamed(Routes.addNewStaff);
         },
-        child: Icon(Icons.add),
+        child: Icon(Icons.add, color: Colors.white),
       ),
     );
   }
